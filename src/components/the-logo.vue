006f09cd2e37a4f2be29edd173348961c288d5d9
filip1139 <filip1139@gmail.com>
2025-02-10 11:14:33 +0100

    refactor: assets/img -> public/img

--- src/components/the-logo.vue
+++ src/components/the-logo.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import logo from "@/assets/img/logo.png";
 import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <RouterLink to="/">
-    <img :src="logo" alt="Site logo" width="140" height="40" />
+    <img src="/img/logo.png" alt="Site logo" width="140" height="40" />
   </RouterLink>
 </template>
